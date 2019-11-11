69ca416d0ff370af4fccb87e72c4c28a5a72d3ca
Walter Weinmann <walter-weinmann@users.noreply.github.com>
2019-11-11 14:48:11 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	priv/ora_bench_result.csv
    #	priv/ora_bench_summary.csv

--- run_bench_travis_push.sh
+++ run_bench_travis_push.sh
@@ -13,25 +13,28 @@ setup_git() {
 
 commit_result_files() {
   git checkout master
-  git fetch -p origin
-  git merge origin/master
+  echo git checkout master
+  git pull
+  echo git pull
   # Current month and year, e.g: Apr 2018
   dateAndMonth=`date "+%b %Y"`
   # Stage the modified files in dist/output
   git add -f priv/ora_bench_result.tsv
+  echo git add -f priv/ora_bench_result.tsv
   git add -f priv/ora_bench_summary.tsv
+  rcho git add -f priv/ora_bench_summary.tsv
   # Create a new commit with a custom build message
   # with "[skip ci]" to avoid a build loop
   # and Travis build number for reference
   git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+  echo git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
 }
 
 upload_files() {
   # Remove existing "origin"
   git remote rm origin
   # Add new "origin" with access token in the git URL for authentication
-  # git remote add origin https://vinaygopinath:${GH_TOKEN}@github.com/vinaygopinath/visa-req-wiki-scraper.git > /dev/null 2>&1
-  git remote add origin https://KonnexionsGmbH:${ORA_BENCH_TOKEN}@github.com/KonnexionsGmbH/ora_bench.git
+  git remote add origin https://KonnexionsGmbH:${ORA_BENCH_TOKEN}@github.com/KonnexionsGmbH/ora_bench.git > /dev/null 2>&1
   git push origin master --quiet
 }
 
