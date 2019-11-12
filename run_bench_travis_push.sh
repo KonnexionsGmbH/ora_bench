d808fea54a88b458980b4e7665f20e03fb982136
Walter Weinmann <walter-weinmann@users.noreply.github.com>
2019-11-12 12:16:10 +0100

    Push vom Travis CI to GitHub.

--- run_bench_travis_push.sh
+++ run_bench_travis_push.sh
@@ -16,6 +16,7 @@ commit_result_files() {
   # Current month and year, e.g: Apr 2018
   dateAndMonth=`date "+%b %Y"`
   # Stage the modified files in dist/output
+  git add -f priv/ora_bench.properties
   git add -f priv/ora_bench_result.tsv
   git add -f priv/ora_bench_summary.tsv
   # Create a new commit with a custom build message
