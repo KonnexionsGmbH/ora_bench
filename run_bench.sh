d6e41561d955b0dbabcfe3777280b872fc7ca370
Walter Weinmann <walter-weinmann@users.noreply.github.com>
2019-11-12 06:01:36 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	priv/ora_bench_result.csv
    #	priv/ora_bench_summary.csv

--- run_bench.sh
+++ run_bench.sh
@@ -12,6 +12,9 @@ sleep .1
 export ORA_BENCH_BENCHMARK_COMMENT='Start with run_bench.sh'
 export ORA_BENCH_CONNECTION_HOST=0.0.0.0
 export ORA_BENCH_CONNECTION_PORT=1521
+if [ -z "$ORA_BENCH_BENCHMARK_ENVIRONMENT" ]; then
+    export ORA_BENCH_BENCHMARK_ENVIRONMENT='local'
+fi
 export ORA_BENCH_FILE_CONFIGURATION_NAME=priv/ora_bench.properties
 
 if [ -z "$ORA_BENCH_JAVA_CLASSPATH" ]; then
@@ -26,6 +29,7 @@ echo "--------------------------------------------------------------------------
 echo "ora_bench - Oracle benchmark - all databases."
 echo "--------------------------------------------------------------------------------"
 echo "BENCHMARK_COMMENT       : $ORA_BENCH_BENCHMARK_COMMENT"
+echo "BENCHMARK_ENVIRONMENT   : $ORA_BENCH_BENCHMARK_ENVIRONMENT"
 echo "CONNECTION_HOST         : $ORA_BENCH_CONNECTION_HOST"
 echo "CONNECTION_PORT         : $ORA_BENCH_CONNECTION_PORT"
 echo "FILE_CONFIGURATION_NAME : $ORA_BENCH_FILE_CONFIGURATION_NAME"
