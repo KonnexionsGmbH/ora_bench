15b584604f88d286a49e503743babe9bd9c15100
Walter Weinmann <walter-weinmann@users.noreply.github.com>
2019-11-23 17:17:22 +0100

    Introducing benchmark series (locally and Travis CI).

--- scripts/run_bench_database_series.sh
+++ scripts/run_bench_database_series.sh
@@ -2,28 +2,9 @@
 
 # ------------------------------------------------------------------------------
 #
-# run_bench_database.sh: Oracle benchmark for a specific database version.
+# run_bench_database_series.sh: Oracle benchmark for a specific database version.
 #
 # ------------------------------------------------------------------------------
-
-if [ -z "$ORA_BENCH_BENCHMARK_DATABASE" ]; then
-    export ORA_BENCH_BENCHMARK_DATABASE=db_19_3_ee
-fi
-if [ -z "$ORA_BENCH_CONNECTION_HOST" ]; then
-    export ORA_BENCH_CONNECTION_HOST=0.0.0.0
-fi
-if [ -z "$ORA_BENCH_CONNECTION_PORT" ]; then
-    export ORA_BENCH_CONNECTION_PORT=1521
-fi
-if [ -z "$ORA_BENCH_CONNECTION_SERVICE" ]; then
-    export ORA_BENCH_CONNECTION_SERVICE=orclcdb
-fi
-if [ -z "$ORA_BENCH_FILE_CONFIGURATION_NAME" ]; then
-    export ORA_BENCH_FILE_CONFIGURATION_NAME=priv/properties/ora_bench.properties
-fi
-if [ -z "$ORA_BENCH_PASSWORD_SYS" ]; then
-    export ORA_BENCH_PASSWORD_SYS=oracle
-fi
 
 if [ -z "$ORA_BENCH_RUN_CX_ORACLE_PYTHON" ]; then
     export ORA_BENCH_RUN_CX_ORACLE_PYTHON=true
@@ -39,16 +20,10 @@ echo "Start $0"
 echo "--------------------------------------------------------------------------------"
 echo "ora_bench - Oracle benchmark - specific database."
 echo "--------------------------------------------------------------------------------"
-echo "BENCHMARK_DATABASE      : $ORA_BENCH_BENCHMARK_DATABASE"
-echo "CONNECTION_HOST         : $ORA_BENCH_CONNECTION_HOST"
-echo "CONNECTION_PORT         : $ORA_BENCH_CONNECTION_PORT"
-echo "CONNECTION_SERVICE      : $ORA_BENCH_CONNECTION_SERVICE"
-echo "FILE_CONFIGURATION_NAME : $ORA_BENCH_FILE_CONFIGURATION_NAME"
+echo "BENCHMARK_DATABASE : $ORA_BENCH_BENCHMARK_DATABASE"
+echo "CONNECTION_SERVICE : $ORA_BENCH_CONNECTION_SERVICE"
 echo "--------------------------------------------------------------------------------"
-echo "RUN_CX_ORACLE_PYTHON    : $ORA_BENCH_RUN_CX_ORACLE_PYTHON"
-echo "RUN_JDBC_JAVA           : $ORA_BENCH_RUN_JDBC_JAVA"
-echo "--------------------------------------------------------------------------------"
-echo "CONNECT_IDENTIFIER      : $ORA_BENCH_CONNECT_IDENTIFIER"
+echo "CONNECT_IDENTIFIER : $ORA_BENCH_CONNECT_IDENTIFIER"
 echo "--------------------------------------------------------------------------------"
 date +"DATE TIME : %d.%m.%Y %H:%M:%S"
 echo "================================================================================"
@@ -63,7 +38,15 @@ while [ "`docker inspect -f {{.State.Health.Status}} ora_bench_db`" != "healthy"
 
 priv/oracle/sqlcl/bin/sql sys/$ORA_BENCH_PASSWORD_SYS@$ORA_BENCH_CONNECT_IDENTIFIER AS SYSDBA @scripts/run_bench_database.sql
 
-{ /bin/bash scripts/run_bench_setup.sh; }
+if [ "$ORA_BENCH_RUN_CX_ORACLE_PYTHON" = "true" ]; then
+    { /bin/bash scripts/run_bench_cx_oracle_python.sh; }
+fi
+
+if [ "$ORA_BENCH_RUN_JDBC_JAVA" = "true" ]; then
+    { /bin/bash scripts/run_bench_jdbc_java.sh; }
+fi
+
+export ORA_BENCH_BENCHMARK_BATCH_SIZE=0
 
 if [ "$ORA_BENCH_RUN_CX_ORACLE_PYTHON" = "true" ]; then
     { /bin/bash scripts/run_bench_cx_oracle_python.sh; }
@@ -73,7 +56,27 @@ if [ "$ORA_BENCH_RUN_JDBC_JAVA" = "true" ]; then
     { /bin/bash scripts/run_bench_jdbc_java.sh; }
 fi
 
-{ /bin/bash scripts/run_bench_finalise.sh; }
+export ORA_BENCH_BENCHMARK_BATCH_SIZE=
+export ORA_BENCH_BENCHMARK_TRANSACTION_SIZE=0
+
+if [ "$ORA_BENCH_RUN_CX_ORACLE_PYTHON" = "true" ]; then
+    { /bin/bash scripts/run_bench_cx_oracle_python.sh; }
+fi
+
+if [ "$ORA_BENCH_RUN_JDBC_JAVA" = "true" ]; then
+    { /bin/bash scripts/run_bench_jdbc_java.sh; }
+fi
+
+export ORA_BENCH_BENCHMARK_BATCH_SIZE=0
+export ORA_BENCH_BENCHMARK_TRANSACTION_SIZE=0
+
+if [ "$ORA_BENCH_RUN_CX_ORACLE_PYTHON" = "true" ]; then
+    { /bin/bash scripts/run_bench_cx_oracle_python.sh; }
+fi
+
+if [ "$ORA_BENCH_RUN_JDBC_JAVA" = "true" ]; then
+    { /bin/bash scripts/run_bench_jdbc_java.sh; }
+fi
 
 EXITCODE=$?
 
--- scripts/run_bench_setup.sh
+++ scripts/run_bench_setup.sh
@@ -6,43 +6,20 @@
 #
 # ------------------------------------------------------------------------------
 
-if [ -z "$ORA_BENCH_BENCHMARK_DATABASE" ]; then
-    export ORA_BENCH_BENCHMARK_DATABASE=db_19_3_ee
-fi
-if [ -z "$ORA_BENCH_CONNECTION_HOST" ]; then
-    export ORA_BENCH_CONNECTION_HOST=0.0.0.0
-fi
-if [ -z "$ORA_BENCH_CONNECTION_PORT" ]; then
-    export ORA_BENCH_CONNECTION_PORT=1521
-fi
-if [ -z "$ORA_BENCH_CONNECTION_SERVICE" ]; then
-    export ORA_BENCH_CONNECTION_SERVICE=orclcdb
-fi
 if [ -z "$ORA_BENCH_FILE_CONFIGURATION_NAME" ]; then
     export ORA_BENCH_FILE_CONFIGURATION_NAME=priv/properties/ora_bench.properties
 fi
 if [ -z "$ORA_BENCH_JAVA_CLASSPATH" ]; then
     export ORA_BENCH_JAVA_CLASSPATH=".;priv/java_jar/*"
 fi
-if [ -z "$ORA_BENCH_PASSWORD_SYS" ]; then
-    export ORA_BENCH_PASSWORD_SYS=oracle
-fi
-
-export ORA_BENCH_CONNECT_IDENTIFIER=//$ORA_BENCH_CONNECTION_HOST:$ORA_BENCH_CONNECTION_PORT/$ORA_BENCH_CONNECTION_SERVICE
 
 echo "================================================================================"
 echo "Start $0"
 echo "--------------------------------------------------------------------------------"
 echo "ora_bench - Oracle benchmark - setup benchmark run."
 echo "--------------------------------------------------------------------------------"
-echo "BENCHMARK_DATABASE      : $ORA_BENCH_BENCHMARK_DATABASE"
-echo "CONNECTION_HOST         : $ORA_BENCH_CONNECTION_HOST"
-echo "CONNECTION_PORT         : $ORA_BENCH_CONNECTION_PORT"
-echo "CONNECTION_SERVICE      : $ORA_BENCH_CONNECTION_SERVICE"
 echo "FILE_CONFIGURATION_NAME : $ORA_BENCH_FILE_CONFIGURATION_NAME"
 echo "JAVA_CLASSPATH          : $ORA_BENCH_JAVA_CLASSPATH"
-echo "--------------------------------------------------------------------------------"
-echo "CONNECT_IDENTIFIER : $ORA_BENCH_CONNECT_IDENTIFIER"
 echo "--------------------------------------------------------------------------------"
 date +"DATE TIME : %d.%m.%Y %H:%M:%S"
 echo "================================================================================"
@@ -50,8 +27,6 @@ echo "==========================================================================
 EXITCODE="0"
 
 PATH=$PATH:/u01/app/oracle/product/12.2/db_1/jdbc/lib
-
-priv/oracle/sqlcl/bin/sql sys/$ORA_BENCH_PASSWORD_SYS@$ORA_BENCH_CONNECT_IDENTIFIER AS SYSDBA @scripts/run_bench_setup.sql
 
 make -f src_java/Makefile clean
 make -f src_java/Makefile
