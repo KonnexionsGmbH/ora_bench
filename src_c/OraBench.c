6620d42dc2e07c5e093a7f7c0bd27f335c1c0ac7
Bikram Chatterjee <chatterjee@bluewin.ch>
2019-12-12 11:46:33 +0100

    WIP

--- src_c/OraBench.c
+++ src_c/OraBench.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include "dpi.h"
+#ifdef EMBED
+#include "../embed/dpi.c"
+#endif
 
 #define L(_fmt, ...) \
     printf("[%s:%s:%d] "_fmt, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+
+struct
+{
+    char user[256];
+    char password[256];
+    char string[1024];
+} connection;
+
+struct
+{
+    char create[1024];
+    char drop[1024];
+    char insert[1024];
+    char select[1024];
+} sql;
+
+dpiContext *gContext;
+
+// error logging
+#define FE(_m, _e)                                                   \
+    {                                                                \
+        L(                                                           \
+            "FATAL: %s - %.*s (%s: %s)\n",                           \
+            _m, _e.messageLength, _e.message, _e.fnName, _e.action); \
+        exit(1);                                                     \
+    }
+
+#define E(_m)                                                        \
+    {                                                                \
+        dpiErrorInfo _i;                                             \
+        dpiContext_getError(gContext, &_i);                          \
+        L(                                                           \
+            "ERROR: %s - %.*s (%s: %s)\n",                           \
+            _m, _i.messageLength, _i.message, _i.fnName, _i.action); \
+    }
 
 int main(const int argc, const char *argv[])
 {
@@ -23,9 +62,6 @@ int main(const int argc, const char *argv[])
     char *keyp = key;
     char *valp = NULL;
 
-    char connStr[256], connUser[256], connPassword[256];
-    memset(connStr, '\0', sizeof(connStr));
-
     while (ch != EOF)
     {
         if (ch == ' ')
@@ -34,8 +70,11 @@ int main(const int argc, const char *argv[])
         }
         else if (ch != '=' && !valp)
         {
-            *keyp = ch;
-            keyp++;
+            if (ch != '\r' && ch != '\n')
+            {
+                *keyp = ch;
+                keyp++;
+            }
         }
         else if (ch == '=' && !valp)
         {
@@ -54,18 +93,42 @@ int main(const int argc, const char *argv[])
             *valp = '\0';
             valp = NULL;
             if (strcmp("connection.string", key) == 0)
-                strcpy(connStr, val);
+                strcpy(connection.string, val);
             else if (strcmp("connection.user", key) == 0)
-                strcpy(connUser, val);
+                strcpy(connection.user, val);
             else if (strcmp("connection.password", key) == 0)
-                strcpy(connPassword, val);
-            L("key %s, value %s\n", key, val);
+                strcpy(connection.password, val);
+            //L("key %s, value %s\n", key, val);
         }
         ch = getc(fp);
     }
 
-    L("connection.string (connStr) %s (%s, %s)\n", connStr, connUser, connPassword);
+    L("connection.string (connStr) %s (%s, %s)\n", connection.string, connection.user, connection.string);
 
     fclose(fp);
+
+    dpiErrorInfo errorInfo;
+    if (dpiContext_create(DPI_MAJOR_VERSION, DPI_MINOR_VERSION, &gContext,
+                          &errorInfo) < 0)
+        FE("Cannot create DPI context", errorInfo);
+
+    L("context created\n");
+
+    const char userName[] = "scott";
+    const char password[] = "regit";
+    const char connectString[] = "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521)))(CONNECT_DATA=(SERVER=dedicated)(SERVICE_NAME=orclpdb1)))";
+    dpiConn *conn;
+    if (dpiConn_create(
+            gContext,
+            userName, strlen(userName),
+            password, strlen(password),
+            connectString, strlen(connectString),
+            NULL, NULL, &conn) < 0)
+    {
+        E("Unable to create connection");
+        exit(1);
+    }
+    L("connected!\n");
+
     return 0;
 }
--- src_c/Readme.md
+++ src_c/Readme.md
@@ -2,7 +2,7 @@
 ## Windows
 ```cmd
 ora_bench\src_c> nmake -f Makefile.win32
-ora_bench\src_c> .\OraBench.exe ..\priv\properties\ora_bench.properties 
+ora_bench\src_c> ./OraBench.exe ../priv/properties/ora_bench.properties 
 ```
 ## Linux
 _TBD_
