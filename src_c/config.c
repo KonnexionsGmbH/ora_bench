8c8b0c7cdb5727a42ab660bec4946e2543cf77b2
Bikram Chatterjee <chatterjee@bluewin.ch>
2019-12-29 15:31:09 +0100

    WIP

--- src_c/config.c
+++ src_c/config.c
@@ -4,95 +4,168 @@
 #include "global.h"
 #include "config.h"
 
-char gConnHost[1024];
-char gConnPort[1024];
-char gConnUser[1024];
-char gConnPaswd[1024];
-char gConnService[1024];
-char gSqlCreate[1024];
-char gSqlDrop[1024];
-char gSqlInsert[1024];
-char gSqlSelect[1024];
-char gBulkDelim[1024];
-char gBulkHeader[1024];
-char gBulkName[1024];
-char gBenchPartitions[1024];
+int benchmarkBatchSize;
+char *benchmarkComment;
+int benchmarkCoreMultiplier;
+char *benchmarkDatabase;
+char *benchmarkHostName;
+char *benchmarkId;
+int benchmarkNumberCores;
+int benchmarkNumberPartitions;
+char *benchmarkOs;
+int benchmarkTransactionSize;
+int benchmarkTrials;
+char *benchmarkUserName;
 
-#define CONF_CP(_field, _var) (strcmp(#_field, key) == 0) strcpy(_var, val)
+int connectionFetchSize;
+char *connectionHost;
+char *connectionPassword;
+int connectionPort;
+char *connectionService;
+char *connectionUser;
+
+char *fileBulkDelimiter;
+char *fileBulkHeader;
+int fileBulkLength;
+char *fileBulkName;
+int fileBulkSize;
+char *fileConfigurationName;
+char *fileResultDelimiter;
+char *fileResultHeader;
+char *fileResultName;
+
+char *sqlCreate;
+char *sqlDrop;
+char *sqlInsert;
+char *sqlSelect;
+
+#define CONF_STR_CP(_field, _var)                            \
+  (strcmp(_field, key) == 0)                                 \
+  {                                                          \
+    _var = (char *)malloc((strlen(val) + 1) * sizeof(char)); \
+    strcpy(_var, val);                                       \
+  }
+
+#define CONF_INT_CP(_field, _var) (strcmp(_field, key) == 0) _var = atoi(val);
+
 void load_config(const char *file)
 {
-    FILE *fp = fopen(file, "r");
-    if (fp == NULL)
+  FILE *fp = fopen(file, "r");
+  if (fp == NULL)
+  {
+    L("ERROR: Unable to open config file %s\n", file);
+    exit(-1);
+  }
+
+  char ch = getc(fp);
+  char key[256];
+  char val[1024];
+  char *keyp = key;
+  char *valp = NULL;
+
+  while (ch != EOF)
+  {
+    if (ch == ' ' && !valp)
     {
-        L("ERROR: Unable to open config file %s\n", file);
-        exit(-1);
+      // skip white spaces
     }
-
-    char ch = getc(fp);
-    char key[256];
-    char val[1024];
-    char *keyp = key;
-    char *valp = NULL;
-
-    while (ch != EOF)
+    else if (ch != '=' && !valp)
     {
-        if (ch == ' ' && !valp)
-        {
-            // skip white spaces
-        }
-        else if (ch != '=' && !valp)
-        {
-            if (ch != '\r' && ch != '\n')
-            {
-                *keyp = ch;
-                keyp++;
-            }
-        }
-        else if (ch == '=' && !valp)
-        {
-            *keyp = '\0';
-            keyp = NULL;
-            valp = val;
-        }
-        else if (ch != '\r' && ch != '\n' && !keyp)
-        {
-            *valp = ch;
-            valp++;
-        }
-        else if (ch == '\r' || ch == '\n')
-        {
-            keyp = key;
-            *valp = '\0';
-            valp = NULL;
-            if
-                CONF_CP(connection.host, gConnHost);
-            else if
-                CONF_CP(connection.port, gConnPort);
-            else if
-                CONF_CP(connection.user, gConnUser);
-            else if
-                CONF_CP(connection.password, gConnPaswd);
-            else if
-                CONF_CP(connection.service, gConnService);
-            else if
-                CONF_CP(sql.create, gSqlCreate);
-            else if
-                CONF_CP(sql.drop, gSqlDrop);
-            else if
-                CONF_CP(sql.insert, gSqlInsert);
-            else if
-                CONF_CP(sql.select, gSqlSelect);
-            else if
-                CONF_CP(file.bulk.delimiter, gBulkDelim);
-            else if
-                CONF_CP(file.bulk.header, gBulkHeader);
-            else if
-                CONF_CP(file.bulk.name, gBulkName);
-            else if
-                CONF_CP(benchmark.number.partitions, gBenchPartitions);
-        }
-        ch = getc(fp);
+      if (ch != '\r' && ch != '\n')
+      {
+        *keyp = ch;
+        keyp++;
+      }
     }
+    else if (ch == '=' && !valp)
+    {
+      *keyp = '\0';
+      keyp = NULL;
+      valp = val;
+    }
+    else if (ch != '\r' && ch != '\n' && !keyp)
+    {
+      *valp = ch;
+      valp++;
+    }
+    else if (ch == '\r' || ch == '\n')
+    {
+      keyp = key;
+      *valp = '\0';
+      valp = NULL;
+      if
+        CONF_STR_CP("connection.host", connectionHost)
+      else if
+        CONF_INT_CP("connection.port", connectionPort)
+      else if
+        CONF_STR_CP("connection.user", connectionUser)
+      else if
+        CONF_STR_CP("connection.password", connectionPassword)
+      else if
+        CONF_INT_CP("benchmark.batch.size", benchmarkBatchSize)
+      else if
+        CONF_STR_CP("benchmark.comment", benchmarkComment)
+      else if
+        CONF_INT_CP("benchmark.core.multiplier", benchmarkCoreMultiplier)
+      else if
+        CONF_STR_CP("benchmark.database", benchmarkDatabase)
+      else if
+        CONF_STR_CP("benchmark.host.name", benchmarkHostName)
+      else if
+        CONF_STR_CP("benchmark.id", benchmarkId)
+      else if
+        CONF_INT_CP("benchmark.number.cores", benchmarkNumberCores)
+      else if
+        CONF_INT_CP("benchmark.number.partitions", benchmarkNumberPartitions)
+      else if
+        CONF_STR_CP("benchmark.os", benchmarkOs)
+      else if
+        CONF_INT_CP("benchmark.transaction.size", benchmarkTransactionSize)
+      else if
+        CONF_INT_CP("benchmark.trials", benchmarkTrials)
+      else if
+        CONF_STR_CP("benchmark.user.name", benchmarkUserName)
+      else if
+        CONF_INT_CP("connection.fetch.size", connectionFetchSize)
+      else if
+        CONF_STR_CP("connection.host", connectionHost)
+      else if
+        CONF_STR_CP("connection.password", connectionPassword)
+      else if
+        CONF_INT_CP("connection.port", connectionPort)
+      else if
+        CONF_STR_CP("connection.service", connectionService)
+      else if
+        CONF_STR_CP("connection.user", connectionUser)
+      else if
+        CONF_STR_CP("file.bulk.delimiter", fileBulkDelimiter)
+      else if
+        CONF_STR_CP("file.bulk.header", fileBulkHeader)
+      else if
+        CONF_INT_CP("file.bulk.length", fileBulkLength)
+      else if
+        CONF_STR_CP("file.bulk.name", fileBulkName)
+      else if
+        CONF_INT_CP("file.bulk.size", fileBulkSize)
+      else if
+        CONF_STR_CP("file.configuration.name", fileConfigurationName)
+      else if
+        CONF_STR_CP("file.result.delimiter", fileResultDelimiter)
+      else if
+        CONF_STR_CP("file.result.header", fileResultHeader)
+      else if
+        CONF_STR_CP("file.result.name", fileResultName)
+      else if
+        CONF_STR_CP("sql.create", sqlCreate)
+      else if
+        CONF_STR_CP("sql.drop", sqlDrop)
+      else if
+        CONF_STR_CP("sql.insert", sqlInsert)
+      else if
+        CONF_STR_CP("sql.select", sqlSelect)
+    }
+    ch = getc(fp);
+  }
 
-    fclose(fp);
+  fclose(fp);
 }
--- src_c/config.h
+++ src_c/config.h
@@ -1,19 +1,41 @@
 #ifndef _CONFIG_H_
 #pragma once
 
-extern char gConnHost[1024];
-extern char gConnPort[1024];
-extern char gConnUser[1024];
-extern char gConnPaswd[1024];
-extern char gConnService[1024];
-extern char gSqlCreate[1024];
-extern char gSqlDrop[1024];
-extern char gSqlInsert[1024];
-extern char gSqlSelect[1024];
-extern char gBulkDelim[1024];
-extern char gBulkHeader[1024];
-extern char gBulkName[1024];
-extern char gBenchPartitions[1024];
+
+extern int benchmarkBatchSize;
+extern char *benchmarkComment;
+extern int benchmarkCoreMultiplier;
+extern char *benchmarkDatabase;
+extern char *benchmarkHostName;
+extern char *benchmarkId;
+extern int benchmarkNumberCores;
+extern int benchmarkNumberPartitions;
+extern char *benchmarkOs;
+extern int benchmarkTransactionSize;
+extern int benchmarkTrials;
+extern char *benchmarkUserName;
+
+extern int connectionFetchSize;
+extern char *connectionHost;
+extern char *connectionPassword;
+extern int connectionPort;
+extern char *connectionService;
+extern char *connectionUser;
+
+extern char *fileBulkDelimiter;
+extern char *fileBulkHeader;
+extern int fileBulkLength;
+extern char *fileBulkName;
+extern int fileBulkSize;
+extern char *fileConfigurationName;
+extern char *fileResultDelimiter;
+extern char *fileResultHeader;
+extern char *fileResultName;
+
+extern char *sqlCreate;
+extern char *sqlDrop;
+extern char *sqlInsert;
+extern char *sqlSelect;
 
 extern void load_config(const char *);
 
