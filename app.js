6c9e4e655ae0d46fcf65b1344c1b798a86551ff2
Bikram Chatterjee <chatterjee@bluewin.ch>
2020-01-08 00:26:21 +0100

    show/hide columns filters

--- app.js
+++ app.js
@@ -1,54 +1,56 @@
 function main() {
-    $.get(
-        "https://api.github.com/repos/KonnexionsGmbH/ora_bench/contents/results?ref=gh-pages",
-        function (data) {
-            $.each(data, function (key, val) {
-                if (val.name.match(/\.tsv/) !== null) {
-                    show_tsv(val.name, val.download_url);
-                }
-            });
+  $.get(
+    "https://api.github.com/repos/KonnexionsGmbH/ora_bench/contents/results?ref=gh-pages",
+    function (data) {
+      $.each(data, function (key, val) {
+        if (val.name.match(/\.tsv/) !== null) {
+          show_tsv(val.name, val.download_url);
         }
-    )
+      });
+    }
+  )
 }
 
 function show_tsv(name, url) {
-    console.log(name, url);
-    $.get(url, function (data) {
-        var lines = data.match(/[^\r\n]+/g);
-        for (var i = 0; i < lines.length; ++i)
-            lines[i] = lines[i].split("\t");
-        var columns = [];
-        for (var i = 0; i < lines[0].length; ++i)
-            columns.push({ title: lines[0][i], visible: i > 6 });
-        console.debug('columns', columns);
-        lines.shift();
-        console.debug('data', lines);
+  console.log(name, url);
+  $.get(url, function (data) {
+    var lines = data.match(/[^\r\n]+/g);
+    for (var i = 0; i < lines.length; ++i)
+      lines[i] = lines[i].split("\t");
+    var columns = [];
+    var tfoot = $('<tfoot>').appendTo('#result');
+    var tfoottr = $('<tr>').appendTo(tfoot);
+    for (var i = 0; i < lines[0].length; ++i) {
+      columns.push({ title: lines[0][i], visible: i > 6 });
+      tfoottr.append($('<th>'));
+    }
+    lines.shift();
+    $('#result').DataTable({
+      dom: 'Blfrtip',
+      buttons: ['colvis'],
+      data: lines,
+      columns: columns,
+      initComplete: function () {
+        this.api().columns().every(function () {
+          var column = this;
+          var select = $('<select><option value=""></option></select>')
+            .appendTo($(column.footer()).empty())
+            .on('change', function () {
+              var val = $.fn.dataTable.util.escapeRegex(
+                $(this).val()
+              );
 
-        var table = $('#result').DataTable({
-            dom: 'Blfrtip',
-            orderCellsTop: true,
-            fixedHeader: true,
-            buttons: ['colvis'],
-            data: lines,
-            columns: columns
-        });
-
-        $('#result thead tr').clone(true).appendTo('#result thead');
-        $('#result thead tr:eq(1) th').each(function (i) {
-            var title = $(this).text();
-            $(this).html('<input type="text" placeholder="Search ' + title + '" />');
-
-            $('input', this).on('keyup change', function () {
-                if (table.column(i).search() !== this.value) {
-                    console.log("here");
-                    table
-                        .column(i)
-                        .search(this.value)
-                        .draw();
-                }
+              column
+                .search(val ? '^' + val + '$' : '', true, false)
+                .draw();
             });
+          column.data().unique().sort().each(function (d, j) {
+            select.append('<option value="' + d + '">' + d + '</option>')
+          });
         });
+      }
     });
+  });
 }
 
 $(document).ready(main);
