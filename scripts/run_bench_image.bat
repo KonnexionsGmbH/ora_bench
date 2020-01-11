d13f699c9a3b8a49b177f715996de1f796c9d4ae
Walter Weinmann <walter-weinmann@users.noreply.github.com>
2020-01-11 21:46:17 +0100

    Docker image ora_bench_dev.

--- scripts/run_bench_image.bat
+++ scripts/run_bench_image.bat
@@ -2,60 +2,43 @@
 
 rem ----------------------------------------------------------------------------
 rem
-rem run_create_image.bat: Create a specific project based on Ubuntu.
+rem run_bench_image.bat: Create a project specific docker image based on Ubuntu.
 rem
 rem ----------------------------------------------------------------------------
 
 setlocal EnableDelayedExpansion
 
-set DOCKER_HUB_PUSH_DEFAULT=no
-
-if "%1"=="" (
-    set /P DOCKER_HUB_PUSH="Push image to Docker Hub (yes / no) [default: %DOCKER_HUB_PUSH_DEFAULT%] "
-) else (
-    set DOCKER_HUB_PUSH=%1
-)
-
 set REPOSITORY=ora_bench_dev
-if ["%DOCKER_HUB_PUSH%"] EQU [""] (
-    set DOCKER_HUB_PUSH=%DOCKER_HUB_PUSH_DEFAULT%
-)
 
 echo.
 echo Skript %0 is now running
 echo.
-echo You can find the run log in the file run_create_image.log
+echo You can find the run log in the file run_bench_image.log
 echo.
 echo Please wait ...
 echo.
 
-> run_create_image.log 2>&1 (
+> run_bench_image.log 2>&1 (
 
     echo =======================================================================
     echo Start %0
     echo -----------------------------------------------------------------------
-
-    echo Create an image for project %REPOSITORY%
-
+    echo Create the docker image %REPOSITORY%
     echo -----------------------------------------------------------------------
     echo:| TIME
     echo =======================================================================
 
     rem rmdir /Q/S tmp\%REPOSITORY%
 
-    copy priv\docker\dockerfile tmp
-
     docker stop %REPOSITORY%
     docker rm -f %REPOSITORY%
     docker rmi -f %REPOSITORY%
 
-    docker build -t %REPOSITORY% tmp
+    docker build -t %REPOSITORY% priv\docker
 
     docker tag %REPOSITORY% konnexionsgmbh/%REPOSITORY%
 
-    if ["%DOCKER_HUB_PUSH%"] EQU ["yes"]  (
-        docker push konnexionsgmbh/%REPOSITORY%
-    )
+    docker push konnexionsgmbh/%REPOSITORY%
 
     for /F %%I in ('docker images -q -f "dangling=true" -f "label=autodelete=true"') do (docker rmi -f %%I)
 
