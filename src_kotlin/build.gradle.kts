35f3631710d94d7df3ee2f9081300946e39ead1b
walter-weinmann <walter.weinmann@gmail.com>
2021-02-21 11:10:30 +0100

    Execution Documentation.

--- src_kotlin/build.gradle.kts
+++ src_kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     application
-    id("org.jetbrains.dokka") version "1.4.10.2"
+//  id("org.jetbrains.dokka") version "0.11.0-dev-59"
     kotlin("jvm") version "1.4.30"
 }
 
@@ -13,14 +13,14 @@ repositories {
 }
 
 dependencies {
-    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.4.10.2")
+//  dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.4.10.2")
     implementation("com.oracle.database.jdbc:ojdbc11:21.1.0.0")
     implementation("commons-logging:commons-logging:1.2")
     implementation("org.apache.commons:commons-csv:1.8")
+    implementation("org.jetbrains.dokka:kotlin-as-java-plugin:1.4.10.2")
+    implementation("org.jetbrains.kotlin:kotlin-bom:1.4.21-js-fixwa")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.30")
     implementation("org.slf4j:slf4j-log4j12:1.7.30")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.4.21-js-fixwa"))
-
 }
 
 val jar by tasks.getting(Jar::class) {
