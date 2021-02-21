ed0fbdcce85cc894cd28d177ad5901c954038653
walter-weinmann <walter.weinmann@gmail.com>
2021-02-21 12:58:55 +0100

    Execution Documentation.

--- src_kotlin/build.gradle.kts
+++ src_kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     id("org.jetbrains.dokka") version "1.4.0"
-    id('com.github.johnrengelman.shadow') version '6.1.0'
+    id("com.github.johnrengelman.shadow") version "6.1.0"
     kotlin("jvm") version "1.4.30"
 }
 
