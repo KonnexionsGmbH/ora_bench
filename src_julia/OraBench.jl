008848cbbe979e765f50d620b69f04de66fd5b7e
Walter Weinmann <walter.weinmann@gmail.com>
2021-06-30 01:47:37 +0200

    Version 1.1.0

--- src_julia/OraBench.jl
+++ src_julia/OraBench.jl
@@ -2,8 +2,7 @@
 OraBench:
 - Julia version: 1.6.1
 - Author: Konnexions GmbH
-- Date: 2021-06-26
-=#
+- Date: 2021-06-26 =#
 
 import Pkg
 
@@ -25,18 +24,36 @@ using TOML
 # Definition of the global variables.
 # ------------------------------------------------------------------------------
 
+BENCHMARK_BATCH_SIZE::UInt
+BENCHMARK_COMMENT::String
+BENCHMARK_CORE_MULTIPLIER::UInt
+BENCHMARK_DATABASE::String
 BENCHMARK_DRIVER::String
+BENCHMARK_HOST_NAME::String 
+BENCHMARK_ID::String 
 BENCHMARK_LANGUAGE::String
-BENCHMARK_NUMBER_PARTITIONS::UInt32
+BENCHMARK_NUMBER_CORES::UInt
+BENCHMARK_NUMBER_PARTITIONS::Uint 
+BENCHMARK_OS::String 
+BENCHMARK_RELEASE::String
+BENCHMARK_TRANSACTION_SIZE::UInt
+BENCHMARK_TRIALS::UInt 
+BENCHMARK_USER_NAME::String
 
+CONNECTION_FETCH_SIZE::UInt
 CONNECTION_HOST::String
 CONNECTION_PASSWORD::String
-CONNECTION_PORT::UInt32
+CONNECTION_PORT::UInt
 CONNECTION_SERVICE::String
 CONNECTION_USER::String
 
+FILE_BULK_DELIMITER::String
+FILE_BULK_LENGTH::UInt 
+FILE_BULK_NAME::String
+FILE_BULK_SIZE::UInt 
 FILE_CONFIGURATION_NAME_TOML = "priv/properties/ora_bench_toml.properties"
-FILE_RESULT_DELIMITER = ""
+FILE_RESULT_DELIMITER::String
+FILE_RESULT_NAME::String 
 
 IX_DURATION_INSERT_SUM = 4
 IX_DURATION_SELECT_SUM = 5
@@ -44,11 +61,16 @@ IX_LAST_BENCHMARK = 1
 IX_LAST_QUERY = 3
 IX_LAST_TRIAL = 2
 
+SQL_CREATE::String
+SQL_DROP::String 
+SQL_INSERT::String
+SQL_SELECT::String
+
 # ------------------------------------------------------------------------------
 # Creating the database connections.
 # ------------------------------------------------------------------------------
 
-function create_connections(config::Dict)
+function create_connections()
     function_name = string(StackTraces.stacktrace()[1].func)
     @debug "Start " * function_name
 
@@ -68,8 +90,8 @@ function create_connections(config::Dict)
             @info "wwe connection " * string(partition_key) * " open"
         catch reason
             @info "partition_key      =" * string(partition_key)
-            @info "connection_user    =" * connection_user
-            @info "connection_password=" * connection_password
+            @info "connection_user    =" * CONNECTION_USER
+            @info "connection_password=" * CONNECTION_PASSWORD
             @info "connection_string  =" * connection_string
             error(
                 "fatal error: program abort =====> database connect error: '" *
@@ -89,7 +111,6 @@ end
 # ------------------------------------------------------------------------------
 
 function create_result(
-    config,
     result_file,
     measurement_data,
     action,
@@ -113,21 +134,21 @@ function create_result(
 
     write(
         result_file,
-        config["benchmark_release"] *
+        BENCHMARK_RELEASE *
         FILE_RESULT_DELIMITER *
-        config["benchmark_id"] *
+        BENCHMARK_ID *
         FILE_RESULT_DELIMITER *
-        config["benchmark_comment"] *
+        BENCHMARK_COMMENT *
         FILE_RESULT_DELIMITER *
-        config["benchmark_host_name"] *
+        BENCHMARK_HOST_NAME *
         FILE_RESULT_DELIMITER *
-        string(config["benchmark_number_cores"]) *
+        string(BENCHMARK_NUMBER_CORES) *
         FILE_RESULT_DELIMITER *
-        config["benchmark_os"] *
+        BENCHMARK_OS *
         FILE_RESULT_DELIMITER *
-        config["benchmark_user_name"] *
+        BENCHMARK_USER_NAME *
         FILE_RESULT_DELIMITER *
-        config["benchmark_database"] *
+        BENCHMARK_DATABASE *
         FILE_RESULT_DELIMITER *
         BENCHMARK_LANGUAGE *
         FILE_RESULT_DELIMITER *
@@ -137,17 +158,17 @@ function create_result(
         FILE_RESULT_DELIMITER *
         sql_statement *
         FILE_RESULT_DELIMITER *
-        string(config["benchmark_core_multiplier"]) *
+        string(BENCHMARK_CORE_MULTIPLIER) *
         FILE_RESULT_DELIMITER *
-        string(config["connection_fetch_size"]) *
+        string(CONNECTION_FETCH_SIZE) *
         FILE_RESULT_DELIMITER *
-        string(config["benchmark_transaction_size"]) *
+        string(BENCHMARK_TRANSACTION_SIZE) *
         FILE_RESULT_DELIMITER *
-        string(config["file_bulk_length"]) *
+        string(FILE_BULK_LENGTH) *
         FILE_RESULT_DELIMITER *
-        string(config["file_bulk_size"]) *
+        string(FILE_BULK_SIZE) *
         FILE_RESULT_DELIMITER *
-        string(config["benchmark_batch_size"]) *
+        string(BENCHMARK_BATCH_SIZE) *
         FILE_RESULT_DELIMITER *
         action *
         FILE_RESULT_DELIMITER *
@@ -169,24 +190,22 @@ end
 # Creating the result file.
 # ------------------------------------------------------------------------------
 
-function create_result_file(config)
+function create_result_file()
     function_name = string(StackTraces.stacktrace()[1].func)
     @debug "Start " * function_name
 
-    result_file_name = config["file_result_name"]
-
-    touch(result_file_name)
+    touch(RESULT_FILE_NAME)
 
     if not
-        isfile(result_file_name)
+        isfile(RESULT_FILE_NAME)
         error(
             "fatal error: program abort =====> result file '" *
-            result_file_name *
+            RESULT_FILE_NAME *
             "' is missing <=====",
         )
     end
 
-    result_file = open(result_file_name, "a")
+    result_file = open(RESULT_FILE_NAME, "a")
 
     @debug "End   " * function_name
 
@@ -198,20 +217,18 @@ end
 # ------------------------------------------------------------------------------
 
 function create_result_measuring_point_end(
-    config,
     result_file,
     measurement_data,
     action,
-    trial_number = 0,
-    sql_statement = "",
-    sql_operation = "",
+    trial_number=0,
+    sql_statement="",
+    sql_operation="",
 )
     function_name = string(StackTraces.stacktrace()[1].func)
     @debug "Start " * function_name
 
     if action == "query"
         create_result(
-            config,
             result_file,
             measurement_data,
             action,
@@ -222,7 +239,6 @@ function create_result_measuring_point_end(
         )
     elseif action == "trial"
         create_result(
-            config,
             result_file,
             measurement_data,
             action,
@@ -233,7 +249,6 @@ function create_result_measuring_point_end(
         )
     elseif action == "benchmark"
         create_result(
-            config,
             result_file,
             measurement_data,
             action,
@@ -258,7 +273,7 @@ end
 # Recording the results of the benchmark - start processing.
 # ------------------------------------------------------------------------------
 
-function create_result_measuring_point_start_benchmark(config::Dict)
+function create_result_measuring_point_start_benchmark()
     function_name = string(StackTraces.stacktrace()[1].func)
     @debug "Start " * function_name
 
@@ -266,7 +281,7 @@ function create_result_measuring_point_start_benchmark(config::Dict)
 
     measurement_data[IX_LAST_BENCHMARK] = TimeDate(now())
 
-    measurement_data_result_file = (measurement_data, open(config["file_result_name"], "a"))
+    measurement_data_result_file = (measurement_data, open(FILE_RESULT_NAME, "a"))
 
     @debug "End   " * function_name
 
@@ -277,24 +292,21 @@ end
 # Loading the bulk file into memory.
 # ------------------------------------------------------------------------------
 
-function get_bulk_data_partitions(config::Dict)
+function get_bulk_data_partitions()
     function_name = string(StackTraces.stacktrace()[1].func)
     @debug "Start " * function_name
 
-    file_bulk_delimiter = config["file_bulk_delimiter"]
-    file_bulk_size = config["file_bulk_size"]
-
-    if file_bulk_size < BENCHMARK_NUMBER_PARTITIONS
+    if FILE_BULK_SIZE < BENCHMARK_NUMBER_PARTITIONS
         error(
-            "fatal error: program abort =====> size of the bulk file ($(file_bulk_size)) is smaller than the number of partitions ($(BENCHMARK_NUMBER_PARTITIONS)) <=====",
+            "fatal error: program abort =====> size of the bulk file ($(FILE_BULK_SIZE)) is smaller than the number of partitions ($(BENCHMARK_NUMBER_PARTITIONS)) <=====",
         )
     end
 
     bulk_data = DataFrame(
-        CSV.File(config["file_bulk_name"], header = 1, delim = file_bulk_delimiter),
+        CSV.File(FILE_BULK_NAME, header=1, delim=FILE_BULK_DELIMITER),
     )
 
-    partition_size = div(file_bulk_size, BENCHMARK_NUMBER_PARTITIONS)
+    partition_size = div(FILE_BULK_SIZE, BENCHMARK_NUMBER_PARTITIONS)
 
     # ------------------------------------------------------------------------------
     # Loading the bulk file into memory.
@@ -308,8 +320,8 @@ function get_bulk_data_partitions(config::Dict)
 
     for partition_key = 1:BENCHMARK_NUMBER_PARTITIONS
         current_partition_upper = last_partition_upper + partition_size
-        if current_partition_upper > file_bulk_size
-            current_partition_upper = file_bulk_size
+        if current_partition_upper > FILE_BULK_SIZE
+            current_partition_upper = FILE_BULK_SIZE
         end
         bulk_data_partitions[partition_key] =
             last_partition_upper + 1, current_partition_upper
@@ -338,57 +350,51 @@ function get_config()
 
     config_parser::Dict = TOML.parsefile(FILE_CONFIGURATION_NAME_TOML)
 
-    config::Dict = Dict()
-
-    config["benchmark_batch_size"] =
+    BENCHMARK_BATCH_SIZE =
         parse(Int64, config_parser["DEFAULT"]["benchmark_batch_size"])
-    config["benchmark_comment"] = config_parser["DEFAULT"]["benchmark_comment"]
-    config["benchmark_core_multiplier"] =
+    BENCHMARK_COMMENT = config_parser["DEFAULT"]["benchmark_comment"]
+    BENCHMARK_CORE_MULTIPLIER =
         parse(Int64, config_parser["DEFAULT"]["benchmark_core_multiplier"])
-    config["benchmark_database"] = config_parser["DEFAULT"]["benchmark_database"]
-    config["benchmark_host_name"] = config_parser["DEFAULT"]["benchmark_host_name"]
-    config["benchmark_id"] = config_parser["DEFAULT"]["benchmark_id"]
-    config["benchmark_number_cores"] =
+    BENCHMARK_DATABASE = config_parser["DEFAULT"]["benchmark_database"]
+    BENCHMARK_HOST_NAME = config_parser["DEFAULT"]["benchmark_host_name"]
+    BENCHMARK_ID = config_parser["DEFAULT"]["benchmark_id"]
+    BENCHMARK_NUMBER_CORES =
         parse(Int64, config_parser["DEFAULT"]["benchmark_number_cores"])
     BENCHMARK_NUMBER_PARTITIONS = 1
     # wwe   parse(Int64, config_parser["DEFAULT"]["benchmark_number_partitions"])
-    config["benchmark_os"] = config_parser["DEFAULT"]["benchmark_os"]
-    config["benchmark_release"] = config_parser["DEFAULT"]["benchmark_release"]
-    config["benchmark_transaction_size"] =
+    BENCHMARK_OS = config_parser["DEFAULT"]["benchmark_os"]
+    BENCHMARK_RELEASE = config_parser["DEFAULT"]["benchmark_release"]
+    BENCHMARK_TRANSACTION_SIZE =
         parse(Int64, config_parser["DEFAULT"]["benchmark_transaction_size"])
-    config["benchmark_trials"] = parse(Int64, config_parser["DEFAULT"]["benchmark_trials"])
-    config["benchmark_user_name"] = config_parser["DEFAULT"]["benchmark_user_name"]
+    BENCHMARK_TRIALS = parse(Int64, config_parser["DEFAULT"]["benchmark_trials"])
+    BENCHMARK_USER_NAME = config_parser["DEFAULT"]["benchmark_user_name"]
 
-    config["connection_fetch_size"] =
+    CONNECTION_FETCH_SIZE =
         parse(Int64, config_parser["DEFAULT"]["connection_fetch_size"])
-    config["connection_host"] = config_parser["DEFAULT"]["connection_host"]
-    config["connection_password"] = config_parser["DEFAULT"]["connection_password"]
-    config["connection_port"] = parse(Int64, config_parser["DEFAULT"]["connection_port"])
-    config["connection_service"] = config_parser["DEFAULT"]["connection_service"]
-    config["connection_user"] = config_parser["DEFAULT"]["connection_user"]
+    CONNECTION_HOST = config_parser["DEFAULT"]["connection_host"]
+    CONNECTION_PASSWORD = config_parser["DEFAULT"]["connection_password"]
+    CONNECTION_PORT = parse(Int64, config_parser["DEFAULT"]["connection_port"])
+    CONNECTION_SERVICE = config_parser["DEFAULT"]["connection_service"]
+    CONNECTION_USER = config_parser["DEFAULT"]["connection_user"]
 
-    config["file_bulk_delimiter"] =
+    FILE_BULK_DELIMITER =
         replace(config_parser["DEFAULT"]["file_bulk_delimiter"], "TAB" => "\t")
-    config["file_bulk_length"] = parse(Int64, config_parser["DEFAULT"]["file_bulk_length"])
-    config["file_bulk_name"] = config_parser["DEFAULT"]["file_bulk_name"]
-    config["file_bulk_size"] = parse(Int64, config_parser["DEFAULT"]["file_bulk_size"])
-    config["file_configuration_name_python"] =
-        config_parser["DEFAULT"]["file_configuration_name_python"]
+    FILE_BULK_LENGTH = parse(Int64, config_parser["DEFAULT"]["file_bulk_length"])
+    FILE_BULK_NAME = config_parser["DEFAULT"]["file_bulk_name"]
+    FILE_BULK_SIZE = parse(Int64, config_parser["DEFAULT"]["file_bulk_size"])
     FILE_RESULT_DELIMITER =
         replace(config_parser["DEFAULT"]["file_result_delimiter"], "TAB" => "\t")
-    config["file_result_name"] = config_parser["DEFAULT"]["file_result_name"]
+    FILE_RESULT_NAME = config_parser["DEFAULT"]["file_result_name"]
 
-    config["sql_create"] = config_parser["DEFAULT"]["sql_create"]
-    config["sql_drop"] = config_parser["DEFAULT"]["sql_drop"]
-    config["sql_insert"] = replace(
+    SQL_CREATE = config_parser["DEFAULT"]["sql_create"]
+    SQL_DROP = config_parser["DEFAULT"]["sql_drop"]
+    SQL_INSERT = replace(
         replace(config_parser["DEFAULT"]["sql_insert"], ":key" => ":1"),
         ":data" => ":2",
     )
-    config["sql_select"] = config_parser["DEFAULT"]["sql_select"]
+    SQL_SELECT = config_parser["DEFAULT"]["sql_select"]
 
     @debug "End   " * function_name
-
-    return config
 end
 
 # ------------------------------------------------------------------------------
@@ -434,24 +440,24 @@ function run_benchmark()
     function_name = string(StackTraces.stacktrace()[1].func)
     @debug "Start " * function_name
 
-    config::Dict = get_config()
+    get_config()
 
     measurement_data_result_file::Tuple =
-        create_result_measuring_point_start_benchmark(config)
+        create_result_measuring_point_start_benchmark()
 
     measurement_data = measurement_data_result_file[1]
     result_file = measurement_data_result_file[2]
 
-    bulk_data, bulk_data_partitions = get_bulk_data_partitions(config)
+    bulk_data, bulk_data_partitions = get_bulk_data_partitions()
 
-    connections = create_connections(config)
+    connections = create_connections()
 
     for partition_key = 1:BENCHMARK_NUMBER_PARTITIONS
         Oracle.close(connections[partition_key])
         @info "connection " * string(partition_key) * " closed"
     end
 
-    create_result_measuring_point_end(config, result_file, measurement_data, "benchmark")
+    create_result_measuring_point_end(result_file, measurement_data, "benchmark")
 
     @debug "End   " * function_name
 end
