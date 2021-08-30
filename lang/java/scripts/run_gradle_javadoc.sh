0d16971f56193f98946f8ad3d0fd62eaab8245d1
Walter Weinmann <walter.weinmann@gmail.com>
2021-08-30 06:26:38 +0200

    Restructuring whole project.

--- lang/java/scripts/run_gradle_javadoc.sh
+++ lang/java/scripts/run_gradle_javadoc.sh
@@ -1,51 +1,34 @@
 #!/bin/bash
 
-set -e
-
 # ------------------------------------------------------------------------------
 #
-# run_application_setup.sh: Setup the db_seeder application.
+# run_gradle.bat: create the Java documentation.
 #
 # ------------------------------------------------------------------------------
-
-export HOME_DB_SEEDER=/db_seeder
 
 echo "================================================================================"
 echo "Start $0"
 echo "--------------------------------------------------------------------------------"
-echo "Setup the DDErl application."
+echo "ora_bench - Oracle benchmark - Gradle: create the Java documentation."
 echo "--------------------------------------------------------------------------------"
 date +"DATE TIME : %d.%m.%Y %H:%M:%S"
 echo "================================================================================"
 
-if [ ! -d "${HOME_DB_SEEDER}" ]; then
-    echo "Create the db_seeder repository"
-    echo "---------------------------------------------------------"
-    git clone https://github.com/KonnexionsGmbH/db_seeder.git
-    cd "${HOME_DB_SEEDER}"
+(
+    cd lang/java || exit
+    
+    if ! { gradle javadoc --warning-mode all; }; then
+        exit 255
+    fi
 
-    echo "Prepare the db_seeder repository"
-    echo "---------------------------------------------------------"
-#    cd "${HOME_DB_SEEDER}"
-    chmod +x *.sh
-    chmod +x lib/*.jar
-    chmod +x scripts/*.sh
-    gradle copyJarToLib
-else
-    cd "${HOME_DB_SEEDER}"
-fi
+    rm -rf ../priv/docs_java
+    mkdir ../priv/docs_java
+    cp -R build/docs/* ../priv/docs_java
+)
 
-echo "Run run_db_seeder_docker_compose"
-echo "---------------------------------------------------------"
-if ! ( ./scripts/run_db_seeder_docker_compose.sh ); then
-    exit 255
-fi
-
-echo " "
+echo ""
 echo "--------------------------------------------------------------------------------"
 date +"DATE TIME : %d.%m.%Y %H:%M:%S"
 echo "--------------------------------------------------------------------------------"
 echo "End   $0"
 echo "================================================================================"
-
-exit
