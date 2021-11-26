0627d2c36e91d392fb9f7b7f2fbf0846142e6290
Walter Weinmann <walter.weinmann@gmail.com>
2021-11-26 14:35:02 +0100

    EOL handling.

--- lang/kotlin/build.gradle.kts
+++ lang/kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.jetbrains.dokka") version "1.5.0"
-    kotlin("jvm") version "1.5.30"
+    id("org.jetbrains.dokka") version "1.6.0"
+    kotlin("jvm") version "1.6.0"
     application
 }
 
@@ -15,11 +15,11 @@ repositories {
 }
 
 dependencies {
-    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.5.31")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.0")
     implementation("com.oracle.database.jdbc:ojdbc11:21.3.0.0")
     implementation("org.apache.commons:commons-csv:1.9.0")
     implementation("org.apache.commons:commons-math3:3.6.1")
-    implementation("org.jetbrains.dokka:kotlin-as-java-plugin:1.5.31")
+    implementation("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.0")
     implementation("org.apache.logging.log4j:log4j-api:2.14.1")
     implementation("org.apache.logging.log4j:log4j-core:2.14.1")
